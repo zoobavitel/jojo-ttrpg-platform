e142749f3690bd450e58d0d6495558d118ce968e
z <criszbav@gmail.com>
2025-07-13 22:36:57 -0400

    chore: Update package dependencies and remove unused files
    - Updated `package.json` and `package-lock.json` to reflect new dependencies and versions.
    - Removed obsolete `frontend/package-lock.json` and related files to streamline the project.
    - Enhanced PostCSS configuration with `postcss-flexbugs-fixes` and `postcss-preset-env`.
    - Updated React and React DOM to the latest production versions for improved performance and stability.
    - Cleaned up unused files in the `frontend/src` directory to maintain a cleaner codebase.

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -3,5 +3,11 @@ module.exports = {
     require('postcss-flexbugs-fixes'),
     require('tailwindcss'),
     require('autoprefixer'),
+    require('postcss-preset-env')({
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+      stage: 3,
+    }),
   ],
 };
